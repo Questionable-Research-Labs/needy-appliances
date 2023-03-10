f7beb22ec5c860581a40f5f85ded86d57425d55f
InvalidSE <taine.reader@gmail.com>
2023-03-10 22:12:09 +1300

    commit messages ratio

--- voice-engine/sst.py
+++ voice-engine/sst.py
@@ -1,30 +1,3 @@
 # path: /voice-engine/sst.py
 # This file runs the speech to text engine (Google for now)
 # plug: https://github.com/invalidse
-
-import speech_recognition as sr
-
-def listen():
-
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-
-        print("[ADJUSTING FOR AMBIENT NOISE...]")
-        r.adjust_for_ambient_noise(source, duration=3)
-        print("[SET MINIMUM ENERGY THRESHOLD TO {}]".format(r.energy_threshold))
-
-        print("[LISTENING...]")
-        audio = r.listen(source)
-        try:
-            text = r.recognize_google(audio)
-            print("[USER] {}".format(text))
-            return text
-        except:
-            print("[USER] ...")
-            return False
-        
-if __name__ == "__main__":
-    while True:
-        script = listen()
-        if script:
-            print("You said: {}".format(script)) # This is where you would run the AI engine, but I'm not going to do that here (main.py does that)
--- voice-engine/tts.py
+++ voice-engine/tts.py
@@ -1,1 +1,30 @@
 # path: /voice-engine/tts.py
+# This file runs the text to speech engine
+# plug: https://github.com/invalidse
+
+# This version will use whisper to recognise the user's voice and convert it to text
+
+# Setup whisper
+import whisper
+model = whisper.load_model("base")
+
+# 
+def listen():
+    print("[LISTENING...]")
+    audio = model.listen()
+    try:
+        text = model.recognize(audio)
+        print("[USER] {}".format(text))
+        return text
+    except:
+        print("[USER] ...")
+        return False
+
+# result = model.transcribe("audio.mp3")
+# print(result["text"])
+
+if __name__ == "__main__":
+    while True:
+        script = listen()
+        if script:
+            print("You said: {}".format(script)) # This is where you would run the AI engine, but I'm not going to do that here (main.py does that)
