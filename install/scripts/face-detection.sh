c75146d4b48da43d1e869c9cd01ec7c2556d0f03
Jasper Miller-Waugh <contact@jmw.nz>
2023-03-12 11:26:12 +1300

    Fix python version

--- install/scripts/face-detection.sh
+++ install/scripts/face-detection.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Install dependinces
 sudo apt install python3.9 python3.9-dev python3-pip libopencv-dev cmake
-sudo python -m pip install -r $PROJECT_ROOT/face-detection/requirements.txt
+sudo python3 -m pip install -r $PROJECT_ROOT/face-detection/requirements.txt
 
 # Install systemd service
-sudo cat $PROJECT_ROOT/install/serviceFiles/qrl-face-detection.service | envsubst | sponge /etc/systemd/system/qrl-face-detection.service
+cat $PROJECT_ROOT/install/serviceFiles/qrl-face-detection.service | envsubst | sudo sponge /etc/systemd/system/qrl-face-detection.service
 sudo systemctl daemon-reload
 sudo systemctl enable qrl-face-detection.service
 
--- install/scripts/voice-engine.sh
+++ install/scripts/voice-engine.sh
@@ -4,6 +4,6 @@ sudo apt install python3.9 python3.9-dev python3-pip cmake libasound2-dev
 sudo python3 -m pip install -r $PROJECT_ROOT/voice-engine/requirements.txt
 
 # Install systemd service
-sudo cat $PROJECT_ROOT/install/serviceFiles/qrl-face-detection.service | envsubst | sponge /etc/systemd/system/qrl-face-detection.service
+cat $PROJECT_ROOT/install/serviceFiles/qrl-face-detection.service | envsubst | sudo sponge /etc/systemd/system/qrl-face-detection.service
 sudo systemctl daemon-reload
 sudo systemctl enable qrl-face-detection.service
