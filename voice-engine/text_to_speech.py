0f66cf420471e691f4ac68c673f450ca357dcd15
InvalidSE <taine.reader@gmail.com>
2023-03-11 17:29:42 +1300

    prog ress

--- voice-engine/text_to_speech.py
+++ voice-engine/text_to_speech.py
@@ -2,4 +2,11 @@
 # This file runs the text to speech engine
 # plug: https://github.com/invalidse
 
+from TTS.api import TTS
+import os
+
+print(TTS.list_models())
+# model_name = TTS.list_models()[0]
+# tts = TTS(model_name)
+# wav = tts.tts("This is a test! This is also a test!!", speaker=tts.speakers[0], language=tts.languages[0])
 
